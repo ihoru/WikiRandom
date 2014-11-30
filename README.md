6515101c75d025cfae255bb8f94708eb48048813
ihoru <ihor.polyakov@gmail.com>
2014-11-30 22:35:29 +0300

    Class was totally rewritten. It is possible to make bulk requests now.

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ it
 $wr->getId();
 812464
 
-$wr->getArticleTitle();
+$wr->getTitle();
 5321 Jagras
 
 $wr->getLink();
@@ -94,4 +94,57 @@ array(2) {
       [0]=> string(63) "http://upload.wikimedia.org/wikipedia/commons/8/83/Celestia.png" 
       [1]=> string(76) "http://upload.wikimedia.org/wikipedia/commons/9/9a/Galileo_Gaspra_Mosaic.jpg" 
 }
+```
+###Bulk usage example:
+Previous example is bad in situations, when you need to get more than one random article. And if you are going to get all properties it is becoming worse because of many requests to API (every call make one request).
+
+If you need to get just a couple properties, but load more than one article, you can use this syntax:
+````php
+$wr = new WikiRandom("ru", 5);
+$article_ids = $wr->getIds();
+$titles = $wr->getTitle();
+
+or the same:
+
+$wr = new WikiRandom("ru", false);
+$article_ids = $wr->getNewRandomArticle(5);
+$titles = $wr->getTitle();
+```
+
+Or you can use more optimized function, but with limited properties:
+````php
+$wr = new WikiRandom("ru", false);
+$data = $wr->getBulkData(5, 5, 0, true);
+// requests: 1 + 5 (list and images for every page)
+
+Array
+(
+    [462957] => Array
+        (
+            [page_id] => 462957
+            [title] => Краснер
+            [length] => 229
+            [url] => http://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%B5%D1%80
+            [text] => <p><b>Краснер</b> — фамилия.</p>
+            [images] => Array
+                (
+                )
+
+        )
+
+    [14836] => Array
+        (
+            [page_id] => 14836
+            [title] => Выбор России (фракция)
+            [length] => 27708
+            [url] => http://ru.wikipedia.org/wiki/%D0%92%D1%8B%D0%B1%D0%BE%D1%80_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8_(%D1%84%D1%80%D0%B0%D0%BA%D1%86%D0%B8%D1%8F)
+            [text] =>
+            [images] => Array
+                (
+                )
+
+        )
+
+)
+
 ```
--- WikiRandom.class.php
+++ WikiRandom.class.php
@@ -1,135 +1,165 @@
 <?php
 
-class WikiRandom{
-	
-	/* ATTRIBUTI */		
-	private $language_supported = array("de", "en", "es", "fr", "it", "nl", "pl" , "ru" , "ceb", "sv", "vi" , "war");
-	private $base_api;
-	private $language;
-        private $article_id;
+class WikiRandom {
+    private $language_supported = array('de', 'en', 'es', 'fr', 'it', 'nl', 'pl' , 'ru' , 'ceb', 'sv', 'vi' , 'war');
+    private $base_api;
+    private $language;
+    private $article_ids = array();
 
+    public function __construct($language, $number = 1) {
+        $this->setLanguage($language);
+        if ($number > 0) {
+            $this->getNewRandomArticle($number);
+        }
+    }
 
-	public function __construct($language){
-                    if (in_array($language, $this->language_supported)) {
-                        $this->language = $language;
-                        
-                        $this->set_base_api($this->language);
-                                                
-                        $this->getNewRandomArticle();
-                    } else {
-                        throw new Exception("Language " . $language . " is not supported");
-                    }                      
-        }
-        
-        private function set_base_api($language){
-            $this->base_api = "http://$language.wikipedia.org/w/api.php?";
-        }
-        
-        public function getApiLanguage(){
-            return $this->language;
-        }
-        
-        public function getNewRandomArticle()
-        {
-            $wiki_api = $this->base_api."action=query&list=random&rnnamespace=0&format=json";
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-            
-            $this->article_id = $json_wapi['query']['random'][0]['id'];                                                                
-        }
-        
-        public function getId()
-        {                                
-            return $this->article_id;
-        }
-        
-        public function getTitle()
-        {
-            $wiki_api = $this->base_api."action=query&prop=info&format=json&pageids=$this->article_id";
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-
-            return $json_wapi['query']['pages'][$this->article_id]['title']; 
-        }
-               
-        public function getLink()
-        {                
-            $wiki_api = $this->base_api."action=query&prop=info&pageids=$this->article_id&inprop=url&format=json";
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-
-            return trim($json_wapi['query']['pages'][$this->article_id]['fullurl']);            
-        }
-        
-        public function getFirstSentence()
-        {               
-            $wiki_api = $this->base_api."action=query&pageids=$this->article_id&prop=extracts&exsentences=1&explaintext=&exsectionformat=plain&format=json";
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-
-            return $json_wapi['query']['pages'][$this->article_id]['extract'];                                
-        }
-        
-        public function getPlainTextArticle()
-        {
-            $wiki_api = $this->base_api."action=query&prop=extracts&exlimit=1&explaintext=&exsectionformat=plain&format=json&pageids=$this->article_id";
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-
-            return $json_wapi['query']['pages'][$this->article_id]['extract'];                                
-        }
-                
-        public function getNChar($number_char)
-        {                
-            $wiki_api = $this->base_api."action=query&pageids=$this->article_id&prop=extracts&explaintext=&exchars=$number_char&format=json";
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-
-            return $json_wapi['query']['pages'][$this->article_id]['extract'];
-        }
-        
-        public function getCategoriesRelated()
-        {                
-            $wiki_api = $this->base_api."action=query&pageids=$this->article_id&prop=categories&format=json";         
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-
-            $all_categories = $json_wapi['query']['pages'][$this->article_id]['categories'];
-
-            $category = array();
-
-            foreach ($all_categories as $key => $value) {                                                      
-                    foreach ($value as $v => $val) {
-                        if ($v != "ns"){                                
-                            $category[] = ltrim(strstr ($val, ":"), ":");                                
-                        }                                
-                    }
+    private function get_list_or_item($data, $field, $i = null, $default = '') {
+        $i = $i === false || count($this->article_ids) > 1 ? $i : 0;
+        if ($i === null || $i === false) {
+            $result = array();
+            foreach ($data as $page_id => $item) {
+                $result[$page_id] = isset($item[$field]) ? $item[$field] : $default;
             }
-
-            return $category;
+            return $result;
         }
-        
-        public function getArticleImages()
-        {             
-                $wiki_api = $this->base_api."action=query&prop=imageinfo&pageids=$this->article_id&generator=images&iiprop=url&format=json";
-                $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);                                
-                                                
-                $url_image = array();
-                
-                foreach ($json_wapi["query"]["pages"] as $key => $value){                    
-                    if ($key < 0)
-                        $url_image[] =  $json_wapi["query"]["pages"][$key]["imageinfo"][0]["url"];
+        return isset($data[$this->article_ids[$i]][$field]) ? $data[$this->article_ids[$i]][$field] : $default;
+    }
+
+    public function getApiLanguage() {
+        return $this->language;
+    }
+
+    public function getSupportedLanguages() {
+        return $this->language_supported;
+    }
+
+    public function setLanguage($language) {
+        if (!in_array($language, $this->getSupportedLanguages())) {
+            throw new Exception(sprintf('Language %s is not supported', $language));
+        }
+        $this->language = $language;
+        $this->base_api = sprintf('http://%s.wikipedia.org/w/api.php?format=json&rawcontinue=1&', $language);
+        return true;
+    }
+
+    public function getNewRandomArticle($number = 1) {
+        $wiki_api = $this->base_api.'action=query&list=random&rnnamespace=0&rnlimit='.$number;
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        $this->article_ids = array();
+        foreach ($json_wapi['query']['random'] as $item) {
+            $this->article_ids[] = $item['id'];
+        }
+        return $this->article_ids;
+    }
+
+    public function getId() {
+        if (empty($this->article_ids)) throw new Exception('Empty article_ids variable');
+        return implode('|', $this->article_ids);
+    }
+
+    public function getIds() {
+        return $this->article_ids;
+    }
+
+    public function getTitle($i = null) {
+        $wiki_api = $this->base_api.'action=query&prop=info&pageids='.$this->getId();
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        return $this->get_list_or_item($json_wapi['query']['pages'], 'title', $i);
+    }
+
+    public function getLink($i = null) {
+        $wiki_api = $this->base_api.'action=query&prop=info&inprop=url&pageids='.$this->getId();
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        return $this->get_list_or_item($json_wapi['query']['pages'], 'fullurl', $i);
+    }
+
+    public function getFirstSentence($number = 1, $i = null) {
+        $wiki_api = sprintf($this->base_api.'action=query&prop=extracts&exsentences=%d&explaintext=&exsectionformat=plain&pageids=%s', intval($number), $this->getId());
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        return $this->get_list_or_item($json_wapi['query']['pages'], 'extract', $i);
+    }
+
+    public function getPlainTextArticle($i = null) {
+        $wiki_api = $this->base_api.'action=query&prop=extracts&exlimit=1&explaintext=&exsectionformat=plain&pageids='.$this->getId();
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        return $this->get_list_or_item($json_wapi['query']['pages'], 'extract', $i);
+    }
+
+    public function getNChar($number = 200, $i = null) {
+        $wiki_api = sprintf($this->base_api.'action=query&prop=extracts&exchars=%d&pageids=%s', intval($number), $this->getId());
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        return $this->get_list_or_item($json_wapi['query']['pages'], 'extract', $i);
+    }
+
+    public function getCategoriesRelated($i = null) {
+        $wiki_api = $this->base_api.'action=query&prop=categories&pageids='.$this->getId();
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        $result = $this->get_list_or_item($json_wapi['query']['pages'], 'categories', false, array());
+        $categories = array();
+        foreach ($result as $page_id => $items) {
+            $categories[$page_id] = array();
+            foreach ($items as $item) {
+                $categories[$page_id][] = ltrim(strstr($item['title'], ':'), ':');
+            }
+        }
+        return $i === null ? $categories : $categories[$this->article_ids[$i]];
+    }
+
+    public function getArticleImages($i = null) {
+        $result = array();
+        $i = count($this->article_ids) > 1 ? $i : 0;
+        $article_ids = $i == null ? $this->article_ids : array($this->article_ids[$i]);
+        foreach ($article_ids as $page_id) {
+            $result[$page_id] = array();
+            $wiki_api = $this->base_api.'action=query&generator=images&prop=imageinfo&iiprop=url&pageids='.$this->getId();
+            $json_wapi = json_decode(file_get_contents($wiki_api), true);
+            foreach ($json_wapi['query']['pages'] as $item) {
+                $result[$page_id][] = $item['imageinfo'][0]['url'];
+            }
+        }
+        return $i === null ? $result : $result[$this->article_ids[$i]];
+    }
+
+    public function getOtherLangLinks($i = null) {
+        $wiki_api = $this->base_api.'action=query&prop=langlinks&llprop=url&pageids='.$this->getId();
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        //return {lang => 'es',url => 'es.wikipedia....', * => 'Titolo'}
+        return $this->get_list_or_item($json_wapi['query']['pages'], 'langlinks', $i, array());
+    }
+
+    public function getBulkData($limit = 1, $sentences = 5, $chars = 200, $with_images = false, $image_min_size = 102400) {
+        $wiki_api = $this->base_api.'action=query&generator=random&grnnamespace=0&grnlimit='.$limit.'&prop=info|extracts&inprop=url';
+        if ($sentences > 0) {
+            $wiki_api .= '&exsentences='.$sentences;
+        } elseif ($chars) {
+            $wiki_api .= '&exchars='.$chars;
+        }
+        $json_wapi = json_decode(file_get_contents($wiki_api), true);
+        $result = array();
+        foreach ($json_wapi['query']['pages'] as $page_id => $item) {
+            $result[$page_id] = array(
+                'page_id' => $page_id,
+                'title' => $item['title'],
+                'length' => $item['length'],
+                'url' => $item['fullurl'],
+                'text' => isset($item['extract']) ? $item['extract'] : '',
+            );
+        }
+        $this->article_ids = array_keys($result);
+        if ($with_images) {
+            foreach ($this->article_ids as $page_id) {
+                $wiki_api = $this->base_api.'action=query&generator=images&prop=imageinfo&iiprop=url|size&pageids='.$page_id;
+                $json_wapi = json_decode(file_get_contents($wiki_api), true);
+                if (!isset($json_wapi['query'])) var_dump($json_wapi);
+                $images = array();
+                foreach ($json_wapi['query']['pages'] as $item) {
+                    $imageinfo = $item['imageinfo'][0];
+                    if ($image_min_size > 0 && $imageinfo['size'] < $image_min_size) continue;
+                    $images[] = $imageinfo['url'];
                 }
-                
-                return $url_image;
-                                                                                                              
-        }
-        
-        public function getOtherLangLinks()
-        {                            
-            $wiki_api = $this->base_api."action=query&prop=langlinks&llprop=url&pageids=$this->article_id&format=json";         
-            $json_wapi = json_decode(file_get_contents($wiki_api),TRUE);
-            
-            //return {lang => "es",url => "es.wikipedia....", * => "Titolo"} 
-            if (array_key_exists("langlinks",$json_wapi['query']['pages'][$this->article_id])){
-                    return $json_wapi['query']['pages'][$this->article_id]['langlinks'];                                                                                   
-            } else {
-                    return NULL;
+                $result[$page_id]['images'] = $images;
             }
-            
         }
-                        
-}      
+        return $result;
+    }
+}
